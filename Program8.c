c7bc70580c542e871b418e6c07ff81d9ed34cea4
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-09 19:40:44 +0530

    Add Program8.c to print numbers in reverse order

---
+++ Program8.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    int n, i;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    for(i = n; i >= 1; i--) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
