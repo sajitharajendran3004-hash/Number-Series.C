22bf106d4ea99a009437140cdeba8c9de2f3e200
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-23 20:43:23 +0530

    Add Armstrong number checker in Program35.c
    This program prompts the user for a number N and prints all Armstrong numbers up to N.

---
+++ Program35.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, temp, rem, sum;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    for(i = 1; i <= n; i++) {
+        temp = i;
+        sum = 0;
+
+        while(temp != 0) {
+            rem = temp % 10;
+            sum = sum + rem * rem * rem;
+            temp = temp / 10;
+        }
+
+        if(sum == i)
+            printf("%d ", i);
+    }
+
+    return 0;
+}
