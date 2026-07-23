d1373bef160983c46f80177c1a8d04f44aae795e
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-23 20:26:49 +0530

    Create Program12.c

---
+++ Program12.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int n, i, sum = 0;
+
+    printf("Enter N: ");
+    scanf("%d", &n);
+
+    for(i = 2; i <= n; i += 2) {
+        sum += i;
+    }
+
+    printf("Sum = %d", sum);
+
+    return 0;
+}
