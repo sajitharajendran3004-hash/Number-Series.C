4f6f6dcfa11923b62cd9f6a94dd7fd113f7d1000
sajitharajendran3004-hash <sajitharajendran3004@gmail.com>
2026-07-23 20:30:49 +0530

    Add program to reverse an integer

---
+++ Program17.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    int num, rem, rev = 0;
+
+    printf("Enter Number: ");
+    scanf("%d", &num);
+
+    while (num != 0) {
+        rem = num % 10;
+        rev = rev * 10 + rem;
+        num = num / 10;
+    }
+
+    printf("Reverse = %d", rev);
+
+    return 0;
+}
